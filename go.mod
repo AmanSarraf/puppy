ca8d6b1dc688f90e34072b33e89032a95c43ac8f
AmanSarraf <amansarrafnit@gmail.com>
2023-12-24 19:38:56 +0530

    imported dog package and used its functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AmanSarraf/puppy
 
 go 1.21.1
+
+require github.com/AmanSarraf/dog v0.0.0-20231224135529-9eabdcac7cf8
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/AmanSarraf/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "woof woof woof !"
+
+}
+func BigBark() string {
+	return dog.WhenGrowsUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowsUp(Barks())
 }
